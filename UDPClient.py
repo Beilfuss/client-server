01b8718c6a3540ca1d4499b628901d7c79d534e8
Beilfuss <93745995+Beilfuss@users.noreply.github.com>
2023-04-16 21:59:06 -0300

    udp implementation working using server

--- UDPClient.py
+++ UDPClient.py
@@ -1,10 +1,26 @@
 from socket import *
 
-serverName = 'hostname'
+'''
+serverName = 'ine5615.matheus.beilfuss.vms.ufsc.br'
 serverPort = 12000
+'''
+
+serverName, serverPort = input('Host/IP Port [Default: localhost 8080]:' ).split()
+
+if serverPort != '':
+    serverPort = int(serverPort)
+
+if serverName == '' and serverPort == '':
+    serverName = 'localhost'
+    serverPort = 8080
+
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input("Input sentence: ")
-clientSocket.sendto(message,(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print("Modified message: ", modifiedMessage)
+
+while 1:
+
+    message = input("Message to send...: ").encode()
+    clientSocket.sendto(message,(serverName, serverPort))
+    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    print("Server reply......: ", modifiedMessage)
+
 clientSocket.close()
--- UDPServer.py
+++ UDPServer.py
@@ -1,13 +1,33 @@
 from socket import *
 
-serverPort = 12000
+# serverPort = 12000
+
+serverName, serverPort = input('Host/IP Port [Default: localhost 8080]: ' ).split()
+
+if serverPort != '':
+    serverPort = int(serverPort)
+
+if serverName == '' and serverPort == '':
+    serverName = 'localhost'
+    serverPort = 8080
+
 serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+print('<<< Socket created >>>')
+
 serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
+
+print('<<< Socket bind complete >>>')
+
+# print("The server is ready to receive")
+
+count = 0
 
 while 1:
 
     message, clientAddress = serverSocket.recvfrom(2048)
 
-    modifiedMessage = message.upper()
+    modifiedMessage = '[{}] OK ::: {}'.format(count, message).encode()
     serverSocket.sendto(modifiedMessage, clientAddress)
+
+    count += 1
